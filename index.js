cf32fb71aff7103ad5d1a1bfd888703c8be7127b
Jessica Lörs <jessica.loers@gmail.com>
2022-05-09 10:28:29 +0200

    add solution ex 1-3

---
+++ index.js
@@ -0,0 +1,31 @@
+// PART 1 -- EXERCISE 1
+const bookmark = document.querySelector('[data-js="bookmark"]');
+
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("card__bookmark--active");
+});
+
+// PART 1 -- EXERCISE 2 / 3
+// const showButton = document.querySelector('[data-js="show__button"]');
+// const answerElement = document.querySelector('[data-js="answer"]');
+
+// showButton.addEventListener("click", () => {
+//   answerElement.toggleAttribute("hidden");
+// });
+
+// PART 1 -- EXERCISE  3
+const showButton = document.querySelector('[data-js="show__button"]');
+const hideButton = document.querySelector('[data-js="hide__button"]');
+const answerElement = document.querySelector('[data-js="answer"]');
+
+showButton.addEventListener("click", () => {
+  hideButton.classList.remove("hide");
+  showButton.classList.add("hide");
+  answerElement.toggleAttribute("hidden");
+});
+
+hideButton.addEventListener("click", () => {
+  hideButton.classList.add("hide");
+  showButton.classList.remove("hide");
+  answerElement.toggleAttribute("hidden");
+});
