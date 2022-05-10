2c11d703811864631301486c04124dfbda62ac40
Jessica Lörs <jessica.loers@gmail.com>
2022-05-10 15:12:17 +0200

    add optional solution excerise 3

--- index.js
+++ index.js
@@ -14,18 +14,32 @@ bookmark.addEventListener("click", () => {
 // });
 
 // PART 1 -- EXERCISE  3
+// const showButton = document.querySelector('[data-js="show__button"]');
+// const hideButton = document.querySelector('[data-js="hide__button"]');
+// const answerElement = document.querySelector('[data-js="answer"]');
+
+// showButton.addEventListener("click", () => {
+//   hideButton.classList.remove("hide");
+//   showButton.classList.add("hide");
+//   answerElement.toggleAttribute("hidden");
+// });
+
+// hideButton.addEventListener("click", () => {
+//   hideButton.classList.add("hide");
+//   showButton.classList.remove("hide");
+//   answerElement.toggleAttribute("hidden");
+// });
+
+// PART 1 -- EXERCISE  3 --optional Solution with single Button
 const showButton = document.querySelector('[data-js="show__button"]');
-const hideButton = document.querySelector('[data-js="hide__button"]');
 const answerElement = document.querySelector('[data-js="answer"]');
 
 showButton.addEventListener("click", () => {
-  hideButton.classList.remove("hide");
-  showButton.classList.add("hide");
-  answerElement.toggleAttribute("hidden");
-});
-
-hideButton.addEventListener("click", () => {
-  hideButton.classList.add("hide");
-  showButton.classList.remove("hide");
-  answerElement.toggleAttribute("hidden");
+  if (showButton.textContent === "Show") {
+    answerElement.toggleAttribute("hidden");
+    showButton.textContent = "Hide";
+  } else {
+    showButton.textContent = "Show";
+    answerElement.toggleAttribute("hidden");
+  }
 });
